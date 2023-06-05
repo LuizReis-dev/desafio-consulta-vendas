88182d29ac48f2a11b8525331cc7089c0fc01306
Luiz Reis <reisluiz05@gmail.com>
2023-06-04 22:00:19 -0300

    GET sale report

--- src/main/java/com/devsuperior/dsmeta/services/SaleService.java
+++ src/main/java/com/devsuperior/dsmeta/services/SaleService.java
@@ -1,8 +1,15 @@
 package com.devsuperior.dsmeta.services;
 
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.ZoneId;
+import java.time.format.DateTimeParseException;
 import java.util.Optional;
 
+import com.devsuperior.dsmeta.dto.SaleReportDTO;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import com.devsuperior.dsmeta.dto.SaleMinDTO;
@@ -19,5 +26,22 @@ public class SaleService {
 		Optional<Sale> result = repository.findById(id);
 		Sale entity = result.get();
 		return new SaleMinDTO(entity);
+	}
+
+    public Page<SaleReportDTO> getReport(String minDate, String maxDate, String name, Pageable pageable) {
+		LocalDate today = LocalDate.ofInstant(Instant.now(), ZoneId.systemDefault());
+		LocalDate min;
+		LocalDate max;
+		try{
+			min = LocalDate.parse(minDate);
+		}catch(DateTimeParseException e){
+			min = today.minusYears(1L);
+		}
+		try{
+			max = LocalDate.parse(maxDate);
+		}catch(DateTimeParseException e){
+			max = today;
+		}
+		return repository.getReport(min, max, name, pageable);
 	}
 }
